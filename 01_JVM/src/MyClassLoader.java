a53799cfa9ac931cbab3cb1f2d1de8798386102e
vuffy <monkeyd.vuffy@gmail.com>
2021-11-07 22:45:37 +0800

    week_01_codes

---
+++ 01_JVM/src/MyClassLoader.java
@@ -0,0 +1,9 @@
+/**
+ * @description: 第一周作业 2.
+ *  定义一个 Classloader，加载一个 Hello.xlass 文件，执行 hello 方法，
+ *  此文件内容是一个 Hello.class 文件所有字节（x=255-x）处理后的文件。
+ * @author: luf
+ * @create: 2021-11-07 19:41
+ **/
+public class MyClassLoader {
+}
